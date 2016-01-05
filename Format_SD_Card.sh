4fa7c68ed88d8350a3b6a26c720ea72cc0c1cad9
Chance <tarver.chance@gmail.com>
2016-01-05 13:32:17 -0600

    Update Format_SD_Card.sh

--- Format_SD_Card.sh
+++ Format_SD_Card.sh
@@ -1,5 +1,5 @@
-dd bs=512 seek=131072 if=files/ of=sdb
-dd bs=512 seek=163840 if=files/ of=sdb
-dd bs=512 seek=196608 if=files/ of=sdb
-dd bs=512 seek=229376 if=files/ of=sdb
-dd bs=512 seek=262144 if=files/ of=sdb
+dd bs=512 seek=131072 if=files/w3_WARPLab_7.7.0_2RF.bin of=sdb
+dd bs=512 seek=163840 if=files/w3_WARPLab_7.7.0_4RF.bin of=sdb
+dd bs=512 seek=196608 if=files/w3_802.11_AP_v1.4.0.bin of=sdb
+dd bs=512 seek=229376 if=files/download_wl74_autodetectEth.bin of=sdb
+
