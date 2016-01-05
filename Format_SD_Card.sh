8bb7fc5b7beb728698f66831a1f8f89948beed41
Chance <tarver.chance@gmail.com>
2016-01-05 10:59:21 -0600

    Create Format_SD_Card.sh

---
+++ Format_SD_Card.sh
@@ -0,0 +1,5 @@
+dd bs=512 seek=131072 if=files/ of=sdb
+dd bs=512 seek=163840 if=files/ of=sdb
+dd bs=512 seek=196608 if=files/ of=sdb
+dd bs=512 seek=229376 if=files/ of=sdb
+dd bs=512 seek=262144 if=files/ of=sdb
