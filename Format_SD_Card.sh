526433a7321429a725c2aec0713ed857df61b7b5
Chance <tarver.chance@gmail.com>
2016-12-15 15:02:49 -0600

    Update .sh to be new bin files for WARPLab 7.7.1

--- Format_SD_Card.sh
+++ Format_SD_Card.sh
@@ -1,5 +1,5 @@
-dd bs=512 seek=131072 if=files/w3_WARPLab_7.7.0_2RF.bin of=/dev/sdb
-dd bs=512 seek=163840 if=files/w3_WARPLab_7.7.0_4RF.bin of=/dev/sdb
+dd bs=512 seek=131072 if=files/w3_WARPLab_7.7.1_2RF.bin of=/dev/sdb
+dd bs=512 seek=163840 if=files/w3_WARPLab_7.7.1_4RF.bin of=/dev/sdb
 dd bs=512 seek=196608 if=files/w3_802.11_AP_v1.4.0.bin of=/dev/sdb
 dd bs=512 seek=229376 if=files/download_wl74_autodetectEth.bin of=/dev/sdb
 dd bs=512 seek=262144 if=files/warpv3_txdco_calibration.bin of=/dev/sdb
